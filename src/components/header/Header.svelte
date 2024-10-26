c3b286ed591239f907194416a363374f93d14c83
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-26 19:56:51 -0300

    Created fist version of header

---
+++ src/components/header/Header.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import HeaderNavigationButton from './HeaderNavigationButton.svelte';
+</script>
+
+<header class="px-8 py-20 flex items-center justify-center space-x-20">
+
+	<div class="flex items-center">
+		<img src="modulite-logo.png" alt="Modu.lite logo" class="w-[220px]" />
+	</div>
+
+	<nav class="flex items-center space-x-8">
+		<HeaderNavigationButton href="#home" title="Home" />
+		<HeaderNavigationButton href="#how-it-works" title="How it works" />
+		<HeaderNavigationButton href="#about-us" title="About us" />
+		<HeaderNavigationButton href="#contacts" title="Contacts" />
+	</nav>
+
+	<a
+		href="#download"
+		class="bg-lemonYellow text-black font-bold px-6 py-3 rounded-full hover:bg-yellow-600 transition duration-200 shadow-inset"
+	>
+		DOWNLOAD MODU.LITE
+	</a>
+</header>
---
+++ src/components/header/HeaderNavigationButton.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let href: string = '#';
+	export let title: string = '';
+</script>
+
+<a href={href} class="relative group font-semibold text-potatoYellow hover:text-lemonYellow transition duration-300">
+	{title}
+	<span class="absolute left-0 bottom-0 w-0 h-[2px] bg-lemonYellow group-hover:w-full transition-all duration-300"></span>
+</a>
