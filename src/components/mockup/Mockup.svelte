3b35b3601c6b1c9cc4a9d829b7921a4d32b1cda4
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-26 21:16:34 -0300

    Created `Mockup` page section

---
+++ src/components/mockup/Mockup.svelte
@@ -0,0 +1,27 @@
+<script lang="ts"></script>
+
+<div class="-mt-[100px] flex flex-col lg:flex-row items-center justify-center text-white lg:space-x-12 min-h-screen px-4">
+	<div class="relative flex items-center justify-center -z-10 mb-8 lg:mb-0">
+		<div class="absolute w-[300px] h-[300px] sm:w-[600px] sm:h-[600px] lg:w-[1000px] lg:h-[1000px] xl:w-[1500px] xl:h-[1500px] rounded-full bg-gradient-radial from-lemonYellow via-transparent to-transparent filter blur-3xl opacity-85"></div>
+
+		<img src="mockup.png" alt="Modu.lite mockup" class="relative w-[200px] sm:w-[300px] lg:w-[441px] z-10" />
+	</div>
+
+	<div class="flex flex-col items-center lg:items-start space-y-8 text-center lg:text-left">
+			<h1 class="text-4xl sm:text-5xl lg:text-6xl font-semibold">
+			<span class="font-spaceGrotesk block mt-4">THE COOLEST</span>
+			<span class="inline-block mt-4 bg-carrotOrange -skew-x-12 py-3 px-4 sm:px-6">
+				<span class="font-jetBrainsMono italic text-white inline-block skew-x-12">
+					dumbfication
+				</span>
+			</span>
+				<span class="font-spaceGrotesk block mt-4">YOU'VE EVER SEEN</span>
+			</h1>
+
+
+		<a href="https://apps.apple.com/us/app/modu-lite/id6667117474" class="inline-block">
+			<img src="appstore-download.png" alt="App Store logo" class="w-[150px] sm:w-[200px] lg:w-[263px]" />
+		</a>
+	</div>
+</div>
+
