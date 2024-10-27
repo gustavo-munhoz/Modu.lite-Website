84d3130fb5bb6748b85bb9b1bb639b141c26b72a
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-26 21:17:44 -0300

    Added fonts and custom functions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,17 @@ export default {
         pageBackground: "#161414",
         lemonYellow: "#E2AE48",
         potatoYellow: "#E9D7AF",
+        carrotOrange: "#CE753B",
+      },
+      fontFamily: {
+        spaceGrotesk: ['"Space Grotesk"', 'sans-serif'],
+        jetBrainsMono: ['"JetBrains Mono"', 'sans-serif']
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+      blur: {
+        '4xl': '200px',
       },
     },
   },
