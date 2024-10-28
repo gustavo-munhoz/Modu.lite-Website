852f08dad4fb29a17a8784b8dfff5565ff4a32d1
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-10-27 23:37:11 -0300

    Added `HowItWorks` component to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script>
 	import Header from '../components/header/Header.svelte';
 	import Mockup from '../components/mockup/Mockup.svelte';
+	import HowItWorks from '../components/howItWorks/HowItWorks.svelte';
 </script>
 
 <Header />
 <div class="pt-[100px]">
 	<Mockup class="relative -mt-20" />
 </div>
+
+<HowItWorks />
